c017e19c2b7690a2d95efbc88eb4dffdf97b1840
Prxfful <praffulhadpad52@gmail.com>
2025-11-05 11:25:25 +0530

    Create create-node.c

---
+++ create-node.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+//node creation by using structure
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* head = NULL;
+int main(){
+    head = (struct node*)malloc(sizeof(struct node));
+    head->data = 8;
+    head->next = NULL;
+    printf("node data is %d",head->data);
+}
+output:
+node data is 8
