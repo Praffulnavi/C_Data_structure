b139c33ff4b422d96f20f187da6057dc92bde6f1
Praffulnavi <praffulhadpad52@gmail.com>
2025-11-07 12:51:27 +0530

    Create stack_push.c

---
+++ stack_push.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX 5
+int stack[MAX];
+int top = -1;
+void push(int value) {
+ if(top == MAX-1) printf("Stack Overflow\n");
+ else {
+ stack[++top] = value;
+ printf("%d pushed\n", value);
+ }
+}
+void display() {
+ if(top == -1) printf("Stack is empty\n");
+ else {
+ printf("Stack (top -> bottom): ");
+ for(int i=top; i>=0; i--)
+ printf("%d ", stack[i]);
+ printf("\n");
+ }
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display();
+    return 0;
+}
